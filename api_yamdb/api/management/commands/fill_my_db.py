57fa0862c9bea0f8062bb6335dab39b949d6b2bd
K_Nasonkin <sticheg2580@gmail.com>
2023-02-16 23:47:56 +0300

    Получилось вытащить данные из csv файлов.

--- api_yamdb/api/management/commands/fill_my_db.py
+++ api_yamdb/api/management/commands/fill_my_db.py
@@ -9,36 +9,40 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Fills DB with data from .csv files"
 
+    FILE_DB_TABLE: dict = {
+        "category.csv": "reviews_category",
+        "comments.csv": "reviews_comments",
+        "genre.csv": "reviews_genre",
+        "genre_title.csv": "reviews_genretitle",
+        "review.csv": "reviews_review",
+        "titles.csv": "reviews_titles",
+        "users.csv": "users_user",
+    }
+    DB_PATH: Path = settings.DATABASES["default"]["NAME"]
+
     def handle(self, *args, **options):
         csv_root: Path = settings.BASE_DIR / "static" / "data"
         files_paths = list(csv_root.glob("*.csv"))
+
+        print(self.DB_PATH)
+
         for file_path in files_paths:
-            self.open_csv(file_path)
+            try:
+                if file_path.name == "review.csv":
+                    self.write_to_db(file_path)
 
-    @staticmethod
-    def open_csv(file_path):
-        with open(file_path, newline="") as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",", quotechar="|")
-            for row in csv_reader:
-                print(", ".join(row))
+            except Exception as er:
+                print("тут", er)
 
-    @staticmethod
-    def write_to_db():
-        con = sqlite3.connect(
-            settings.BASE_DIR / "db.sqlite3"
-        )  # change to 'sqlite:///your_filename.db'
-        cur = con.cursor()
-        cur.execute(
-            "CREATE TABLE t (col1, col2);"
-        )  # use your column names here
+    def write_to_db(self, file_path: Path) -> None:
+        connection = sqlite3.connect(self.DB_PATH)
+        cursor = connection.cursor()
 
-        with open(
-                'data.csv', 'r'
-        ) as fin:  # `with` statement available in 2.5+
-            # csv.DictReader uses first line in file for column headings by default
-            dr = csv.DictReader(fin)  # comma is default delimiter
-            to_db = [(i['col1'], i['col2']) for i in dr]
-
-        cur.executemany("INSERT INTO t (col1, col2) VALUES (?, ?);", to_db)
-        con.commit()
-        con.close()
+        with open(file_path, newline="", encoding="utf-8") as csv_file:
+            dict_reader = csv.DictReader(csv_file)
+            to_db = [i for i in dict_reader]
+        print(to_db.pop().keys())
+        # cursor.executemany(
+        #     """INSERT INTO t (col1, col2) VALUES (?, ?);""", to_db)
+        # connection.commit()
+        connection.close()
