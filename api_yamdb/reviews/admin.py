10e2e21c06d6862e1137f1ec381a2f045a5169f1
K_Nasonkin <sticheg2580@gmail.com>
2023-02-21 15:31:14 +0300

    Лев добавил Админку своих моделей

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Genre, Title
+from .models import Category, Comment, Genre, Review, Title
 
 
 class TitleAdmin(admin.ModelAdmin):
@@ -12,21 +12,29 @@ class TitleAdmin(admin.ModelAdmin):
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "name",
-        "slug",
-    )
+    list_display = ("pk", "name", "slug")
 
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "name",
-        "slug",
-    )
+    list_display = ("pk", "name", "slug")
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "title", "text", "score", "pub_date")
+    search_fields = ("author", "title", "score")
+    list_filter = ("author", "title", "score")
+    empty_value_display = "-пусто-"
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "review", "text", "pub_date")
+    search_fields = ("author", "review")
+    list_filter = ("author", "review")
+    empty_value_display = "-пусто-"
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Title, TitleAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
