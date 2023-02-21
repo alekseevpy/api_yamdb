72f4906250182586c904c6497477508c40beb48b
K_Nasonkin <sticheg2580@gmail.com>
2023-02-21 16:52:25 +0300

    Снова прогнал flake 8 и добавил сортировку Users

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -58,6 +58,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+        ordering = ("id",)
 
     def __str__(self) -> str:
         return self.username
