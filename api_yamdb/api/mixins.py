60a88bfbfbf6f2f297a045b87cc3f705f1c75b86
K_Nasonkin <sticheg2580@gmail.com>
2023-02-21 16:31:31 +0300

    Прогнал black

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,9 +1,14 @@
-from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
-                                   ListModelMixin)
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin,
+)
 from rest_framework.viewsets import GenericViewSet
 
 
-class GetListCreateDeleteMixin(GenericViewSet, CreateModelMixin,
-                               ListModelMixin, DestroyModelMixin):
+class GetListCreateDeleteMixin(
+    GenericViewSet, CreateModelMixin, ListModelMixin, DestroyModelMixin
+):
     """Кастомный класс для жанров и категорий."""
+
     pass
